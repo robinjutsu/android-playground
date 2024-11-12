e4f9499970c85e297643b4ea2dff5c4c8c4be780
robinjutsu <robinjutsu@gmail.com>
2024-11-11 22:45:24 -0600

    add recycler view 1

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -10,14 +10,14 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        val title:TextView = findViewById(R.id.titleTextView)
-        val learnMoreButton: Button = findViewById(R.id.learnMoreButton)
-        learnMoreButton.setOnClickListener {
-            // todo navigate
-            val intent = Intent(it.context, DetailActivity::class.java)
-            val title = title.text.toString()
-            intent.putExtra("title",title)
-            startActivity(intent)
-        }
+//        val title:TextView = findViewById(R.id.titleTextView)
+//        val learnMoreButton: Button = findViewById(R.id.learnMoreButton)
+//        learnMoreButton.setOnClickListener {
+//            // todo navigate
+//            val intent = Intent(it.context, DetailActivity::class.java)
+//            val title = title.text.toString()
+//            intent.putExtra("title",title)
+//            startActivity(intent)
+//        }
     }
 }
