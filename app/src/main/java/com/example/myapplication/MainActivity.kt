fc56dacd8678c559b711539480502d9d400a9473
robinjutsu <robinjutsu@gmail.com>
2024-11-11 21:59:52 -0600

    add cardview

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -1,26 +1,13 @@
 package com.example.myapplication
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.util.Log
-import android.widget.Button
-import android.widget.EditText
-import android.widget.Toast
-import com.google.android.material.snackbar.Snackbar
-import com.google.android.material.textfield.TextInputEditText
+import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        val button: Button = findViewById(R.id.bottomButton)
-        val inputEditText: EditText = findViewById(R.id.inputEditText)
-        button.setOnClickListener {
-            //Toast.makeText(it.context, inputEditText.text.toString(), Toast.LENGTH_SHORT).show()
-            if (inputEditText.text.toString().isEmpty())
-                return@setOnClickListener
-            Snackbar.make(it,inputEditText.text.toString(),Snackbar.LENGTH_SHORT).show()
-        }
+
     }
 }
